24f55593b8f6aa51a321fd97a3f1c4f966b3691c
codevelom <codevelomgroup@gmail.com>
2021-05-25 17:13:32 +0100

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,206 @@
+<footer class="footer-section white-bg pt-100">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="single-footer-widget">
+                     <h3>Contact info</h3>
+                     <div class="footer-info-contact">
+                        <i class="flaticon-pin"></i>
+                        <h3>Location</h3>
+                        <span>No.19, Odejayi Street, <br> Agodi Gate, Ibadan.</span>
+                     </div>
+                     <div class="footer-info-contact">
+                        <i class="flaticon-call"></i>
+                        <h3>Call Us</h3>
+                        <span><a href="tel:+882-569-756">+234 803 382 0951</a></span>
+                     </div>
+                     <div class="footer-info-contact">
+                        <i class="flaticon-email"></i>
+                        <h3>Email Us</h3>
+                        <span>
+                        <a href="mailto:support@cityofgod.netlify.app">
+                        <span class="__cf_email__" data-cfemail="cityofgod.netlify.app">[email&#160;protected]</span>
+                        </a>
+                        </span>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="single-footer-widget">
+                     <h3>Our Support</h3>
+                     <ul class="footer-quick-links">
+                        <li>
+                           <a href="#">
+                           Donate Now
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Become a Volunteer
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Careers
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Contact Us
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Partnership
+                           </a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-2 col-md-6 col-sm-6">
+                  <div class="single-footer-widget">
+                     <h3>Our Services</h3>
+                     <ul class="footer-quick-links">
+                        <li>
+                           <a href="#">
+                           Donate
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Sponsor
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Fundraise
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Volunteer
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Partner
+                           </a>
+                        </li>
+                        <!-- <li>
+                           <a href="#">
+                           Jobs
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                           Form
+                           </a>
+                        </li> -->
+                     </ul>
+                  </div>
+               </div>
+               <!-- <div class="col-lg-4 col-md-6 col-sm-6">
+                  <div class="single-footer-widget">
+                     <h3>Recent News</h3>
+                     <div class="footer-news">
+                        <a href="single-blog.html">
+                           <img src="assets/img/footer-news/1.jpg" alt="image">
+                           <h4>Support about poverty to poor family</h4>
+                           <span>Poor, 22 January</span>
+                        </a>
+                     </div>
+                     <div class="footer-news">
+                        <a href="single-blog.html">
+                           <img src="assets/img/footer-news/2.jpg" alt="image">
+                           <h4>Mostly suffered school Boys care</h4>
+                           <span>Health, 24 February</span>
+                        </a>
+                     </div>
+                  </div>
+               </div> -->
+            </div>
+         </div>
+         <div class="copyright-area">
+            <div class="container">
+               <div class="copyright-area-content">
+                  <div class="row align-items-center">
+                     <div class="col-lg-4">
+                        <div class="copyright-logo">
+                           <img src="assets/cog/logo.png" class="" alt="image">
+                        </div>
+                     </div>
+                     <div class="col-lg-4 col-md-6">
+                        <p>
+                           <i class="far fa-copyright"></i>
+                           Copyright © <?php echo date('Y'); ?> City of God. All Rights Reserved by
+                           <a href="https://codevelom.netlify.app/" target="_blank">
+                           Powered  by Codevelom Technologies
+                           </a>
+                        </p>
+                     </div>
+                     <!-- <div class="col-lg-4 col-md-6">
+                        <ul>
+                           <li>
+                              <a href="terms-condition.html">Terms & Conditions</a>
+                           </li>
+                           <li>
+                              <a href="privacy-policy.html">Privacy Policy</a>
+                           </li>
+                        </ul>
+                     </div> -->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <div class="go-top">
+         <i class="bx bx-chevron-up"></i>
+         <i class="bx bx-chevron-up"></i>
+      </div>
+      <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/js/jquery.meanmenu.js"></script>
+      <script src="assets/js/jquery.easing.min.js"></script>
+      <script src="assets/js/owl.carousel.js"></script>
+      <script src="assets/js/jquery.magnific-popup.min.js"></script>
+      <script src="assets/js/jquery.nice-select.min.js"></script>
+      <script src="assets/js/jquery.appear.js"></script>
+      <script src="assets/js/odometer.min.js"></script>
+      <script src="assets/js/imagelightbox.min.js"></script>
+      <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+      <script src="assets/js/form-validator.min.js"></script>
+      <script src="assets/js/contact-form-script.js"></script>
+      <script src="assets/js/main.js"></script>
+      <script src="assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+      <script src="assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+      <script src="assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+      <script src="assets/js/rev-slider-custom.js"></script>
+
+      <!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+2348033820951", // WhatsApp number
+            call: "+2348033820951", // Call phone number
+            call_to_action: "Send Us Message", // Call to action
+            button_color: "#E74339", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "whatsapp,call", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
+   </body>
---
+++ includes/header.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="zxx">
+   <!-- Mirrored from templates.envytheme.com/huruma/default/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 23 May 2021 10:45:36 GMT -->
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="assets/revolution/css/settings.css">
+      <link rel="stylesheet" href="assets/revolution/css/layers.css">
+      <link rel="stylesheet" href="assets/revolution/css/navigation.css">
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+      <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+      <link rel="stylesheet" href="assets/css/animate.min.css">
+      <link rel="stylesheet" href="assets/css/boxicons.min.css">
+      <link rel="stylesheet" href="assets/css/flaticon.css">
+      <link rel="stylesheet" href="assets/css/imagelightbox.min.css">
+      <link rel="stylesheet" href="assets/css/meanmenu.css">
+      <link rel="stylesheet" href="assets/css/nice-select.min.css">
+      <link rel="stylesheet" href="assets/css/odometer.min.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <link rel="stylesheet" href="assets/css/responsive.css">
+      <title>City of God - Cherubim & Seraphim Church Ibadan.</title>
+      <link rel="icon" type="image/png" href="assets/cog/logo.png">
+   </head>
+   <body>
+      <div class="preloader">
+         <div class="preloader">
+            <span></span>
+            <span></span>
+         </div>
+      </div>
+      <div class="navbar-area  navbar-style-two">
+         <div class="huruma-responsive-nav">
+            <div class="container">
+               <div class="huruma-responsive-menu">
+                  <div class="logo">
+                     <a href="#">
+                     <img src="assets/cog/logo.png" class="white-logo " alt="logo">
+                     <img src="assets/cog/logo.png" class="black-logo " alt="image">
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="huruma-nav">
+            <div class="container">
+               <nav class="navbar navbar-expand-md navbar-light">
+                  <a class="navbar-brand" href="#">
+                  <img src="assets/cog/logo.png" class="white-logo " alt="logo">
+                  <img src="assets/cog/logo.png" class="black-logo " alt="image">
+                  </a>
+                  <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                           <a href="#" class="nav-link">
+                           Home
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="#about-us" class="nav-link">
+                           About
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="#coverage" class="nav-link">
+                           Coverage
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="#mission" class="nav-link">
+                           Mission
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="#testimonial" class="nav-link">
+                           Testimonial
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="#contact" class="nav-link">
+                           Contact
+                           </a>
+                        </li>
+                     </ul>
+                     <div class="others-options">
+                        <!-- <div class="dropdown language-switcher d-inline-block">
+                           <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           <img src="assets/img/flag/france-flag.jpg" class="shadow" alt="image">
+                           <span>Eng <i class='bx bx-chevron-down'></i></span>
+                           </button>
+                           <div class="dropdown-menu">
+                              <a href="#" class="dropdown-item d-flex align-items-center">
+                              <img src="assets/img/flag/germany-flag.jpg" class="shadow-sm" alt="flag">
+                              <span>Ger</span>
+                              </a>
+                              <a href="#" class="dropdown-item d-flex align-items-center">
+                              <img src="assets/img/flag/italy-flag.jpg" class="shadow-sm" alt="flag">
+                              <span>Fre</span>
+                              </a>
+                              <a href="#" class="dropdown-item d-flex align-items-center">
+                              <img src="assets/img/flag/spain-flag.jpg" class="shadow-sm" alt="flag">
+                              <span>Spa</span>
+                              </a>
+                              <a href="#" class="dropdown-item d-flex align-items-center">
+                              <img src="assets/img/flag/russia-flag.jpg" class="shadow-sm" alt="flag">
+                              <span>Rus</span>
+                              </a>
+                              <a href="#" class="dropdown-item d-flex align-items-center">
+                              <img src="assets/img/flag/us-flag.jpg" class="shadow-sm" alt="flag">
+                              <span>Ita</span>
+                              </a>
+                           </div>
+                        </div> -->
+                        <div class="option-item">
+                           <i class="search-btn bx bx-search"></i>
+                           <i class="close-btn bx bx-x"></i>
+                           <div class="search-overlay search-popup">
+                              <div class='search-box'>
+                                 <form class="search-form">
+                                    <input class="search-input" name="search" placeholder="Search" type="text">
+                                    <button class="search-button" type="submit">
+                                    <i class="bx bx-search"></i>
+                                    </button>
+                                 </form>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="burger-menu">
+                           <i class="menu-icon flaticon-menu"></i>
+                        </div>
+                     </div>
+                  </div>
+               </nav>
+            </div>
+         </div>
+      </div>
+      <div class="sidebar-modal">
+         <div class="sidebar-modal-inner">
+            <div class="sidebar-about-area">
+               <div class="title">
+                  <h2>About Us</h2>
+                  <p>We believe brand interaction is key in communication. Real innovations and a positive customer experience are the heart of successful communication.
+                     
+                  </p>
+               </div>
+            </div>
+            <!-- <div class="sidebar-instagram-feed">
+               <h2>Instagram</h2>
+               <ul>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/1.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/2.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/3.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/4.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/5.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/6.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/7.jpg" alt="image">
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#">
+                     <img src="assets/img/sidebar/8.jpg" alt="image">
+                     </a>
+                  </li>
+               </ul>
+            </div> -->
+            <div class="sidebar-contact-area">
+               <div class="contact-info">
+                  <div class="contact-info-content">
+                     <h2>
+                        <a href="tel:+0881306298615">
+                        +234 803 382 0951
+                        </a>
+                        <span>OR</span>
+                        <a href="mailto:support@cityofgod.netlify.app">
+                        <span class="__cf_email__" data-cfemail="cityofgod.netlify.app">[email&#160;protected]</span>
+                        </a>
+                     </h2>
+                     <ul class="social">
+                        <li>
+                           <a href="https://www.facebook.com/CITYOFGODCSMC" target="_blank">
+                           <i class="flaticon-facebook"></i>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" target="_blank">
+                           <i class="flaticon-twitter"></i>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="https://www.instagram.com/cityofgodcmsc" target="_blank">
+                           <i class="flaticon-instagram"></i>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" target="_blank">
+                           <i class="flaticon-linkedin"></i>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" target="_blank">
+                           <i class="flaticon-pinterest"></i>
+                           </a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <span class="close-btn sidebar-modal-close-btn">
+            <i class="flaticon-close"></i>
+            </span>
+         </div>
+      </div>
